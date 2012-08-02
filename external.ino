0331edc2b624651a9cc2e0fb32119334bf51cbc9
Will Clifton <wclifton1@gmail.com>
2012-08-02 15:34:12 -0500

    v2
    external(1) internal2

--- external.ino
+++ external.ino
@@ -1,4 +1,4 @@
-// External Battery (is this really a battery? was this designed for external battery use?)
+// External Battery 
 
 void setup() {
   Serial.begin(9600);
@@ -7,28 +7,28 @@ void setup() {
 void loop() {
   String StringBat = BatteryFunction2(A6);
   Serial.println(String(StringBat));
-  delay(1000);
+  delay(3000);
 } 
 
 String BatteryFunction2(int BatteryPin) {
   int sensorValue = analogRead(BatteryPin);//may need to add float
-  float BatteryVoltage = float(sensorValue)/212;
-  float BattVolt = BatteryVoltage*6; //why extra line?
-  float BatteryLife = float(28.57*BattVolt - 528.5); //float() necessary? how did this formula come about?
-
+  float BatteryVoltage = (float)sensorValue/212;
+  float BattVolt = BatteryVoltage*6;
+  float BatteryLife = float(28.57*BattVolt - 528.5);
+  
   if (BatteryLife < 0)
-    BatteryLife = 0;
+	BatteryLife = 0;
   if (BatteryLife > 100)
-    BatteryLife = 100;
+	BatteryLife = 100;
   char b1[32];
-  dtostrf(BatteryLife, 5, 2, b1); //look up this conversion
+  dtostrf(BatteryLife, 5, 2, b1);
   char b2[32];
   dtostrf(BattVolt, 5, 2, b2);
   //Serial.print("Sensor Value =");
   //Serial.println(sensorValue);
 
-  String s = String(b1) + ", ";
-  s= s + String(b2); //currently outputting blank?
+  String s = "#ex,"+String(b1) + ",";
+  s= s + String(b2)+",<!MSG>";
   return(s);
 }
 
--- internal.ino
+++ internal.ino
@@ -8,13 +8,15 @@ const int FREQ_PIN = A5;
 const int BAT_PIN = A3;
 const int STATUS_PIN = A6;
 
-#define SOP '<'
-#define EOP '>'
+char SOP ='<';
+char EOP ='>';
+
 boolean started = false;
 boolean ended = false;
 char inData[80];
 byte index;
-String stringFinal = ""; //not needed?
+
+String stringFinal = "";
 
 String msg = "";
 String RPM = "";
@@ -33,9 +35,11 @@ void setup() {
 // Main loop
 void loop() {
   sensorValue = analogRead(BAT_PIN); 
+
   //read all serial data available, as fast as possible
   while(Serial.available() > 0){
     char inChar = Serial.read();
+    //Serial.println(inChar);
     if(inChar == SOP) {
       index=0;
       inData[index] = '\0';
@@ -50,54 +54,56 @@ void loop() {
       if (index < 79) {
         inData[index] = inChar;
         index++;
-        inData[index[ = '\0';
+        inData[index] = '\0';
       }
     }
   }
+  //we are here b/c end of packet arrived OR all data read
+  if(started && ended) {
+    //complete packet, process
+    Serial.println(inData);
+    analogWrite(PWM_PIN, inData[0]); // make pwm signal (0-255)
 
-    PWMSignal = Serial.read();
-
-    analogWrite(PWM_PIN, PWMSignal); // make pwm signal (0-255)
-
+    //reset for next packet
+    started = false;
+    ended = false;
+    index = 0;
+    inData[index] = '\0';
   }
-else {
-  otherSignal = Serial.read();
+  if (Serial.available() == 0) {
+    msg = DeviceCoupling(STATUS_PIN);
+    RPM = getFrequency(FREQ_PIN);
+    bat = String(BatteryFunction());
+    batvolt = String(BatteryVoltage());
 
-  Serial.flush();
-}
+    stringFinal = "";
+    stringFinal += SOP;
+    stringFinal += "in"; //for parsing source
+    stringFinal += ",";
+    stringFinal += bat;
+    stringFinal += ",";
+    stringFinal += batvolt;
+    stringFinal += ",";
+    stringFinal += RPM;
+    stringFinal += ",";
+    stringFinal += msg;
+    stringFinal += EOP;
+    stringFinal +='.'; //extra
 
-
-msg = DeviceCoupling(STATUS_PIN);
-RPM = getFrequency(FREQ_PIN);
-bat = String(BatteryFunction());
-batvolt = String(BatteryVoltage());
-
-stringFinal = "";
-
-stringFinal += "#"; //signals new message, so not interrupted
-stringFinal += "in"; //for parsing source
-stringFinal += ",";
-stringFinal += bat;
-stringFinal += ",";
-stringFinal += batvolt;
-stringFinal += ",";
-stringFinal += RPM;
-stringFinal += ",";
-stringFinal += msg;
-stringFinal += "<!MSG>";
-
-char s1[stringFinal.length()+1]; //must accomodate the terminating 0, weird i know
-stringFinal.toCharArray(s1, stringFinal.length()+1);
-Serial.println(s1);
+    char s1[stringFinal.length()];
+    stringFinal.toCharArray(s1, stringFinal.length());
+    Serial.println(s1);
+  }
 }
 
 //analog measurement function
-String DeviceCoupling(int AnalogInPin) { //Voltage is > 1 means coupled
+String DeviceCoupling(int AnalogInPin) { //Voltage is > 1.5 means coupled
   float Voltage = 0;
   String msg;
   int sensorValue = analogRead(AnalogInPin);
   Voltage = (float)sensorValue/214;
-  if(Voltage > 1)
+  Serial.println(Voltage);
+  if(Voltage > 1.5)
     msg = "1";
   else
     msg = "0";
@@ -119,7 +125,7 @@ String BatteryFunction() {
   float BatteryVoltage;
 
   BatteryVoltage = (float)sensorValue/214;
-  BatteryLife= (111 * BatteryVoltage - 310);
+  BatteryLife= (111 * BatteryVoltage - 310); // 2.8V-3.7V is 0-100%
   if (BatteryLife < 0) 
     BatteryLife = 0;
   if (BatteryLife > 100)
@@ -140,8 +146,7 @@ String getFrequency(int FREQ_PIN) {
   String rpmout = "";
   for(int j=0; j<samples; j++) pulse1+= pulseIn(FREQ_PIN, HIGH, 40000);
   for(int j=0; j<samples; j++) pulse2+= pulseIn(FREQ_PIN, LOW, 40000); //this timeout delays the whole loop, previously 250000
-
-    pulse = pulse1 + pulse2;
+  pulse = pulse1 + pulse2;
   pulse = pulse/10;
   //float temprpm = float(33012-.7401*pulse);
   float temprpm = float(60*1000000/pulse); //pulse units are us/cycle, cycles/us=1/pulse, 1000000/pulse=cycles/s, 60*1000000/pulse=rpm
@@ -154,21 +159,8 @@ String getFrequency(int FREQ_PIN) {
     rpm = rpm/1000;
     rpmout = String(rpm)+"000";
   }
-  delay(500); //also delays the write speed
   return(rpmout);
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
